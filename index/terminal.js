f27801a56ad8371be29ca6ba3febe45f1faf43bf
HerokeyVN <tuoicho2006@gmail.com>
2021-10-28 02:58:51 +0000

    Add send text to CMD

--- index/terminal.js
+++ index/terminal.js
@@ -10,6 +10,19 @@ ipc.send("terminal.toterm", `cls \x0D`);
 	ipc.send("terminal.toterm", e);
 })*/
 
+
+
+var sendbtn = document.querySelector('.sendcmd');
+
+
+sendbtn.onclick = function (){
+	var sendvl = document.getElementById(`cmd`).value
+	if (sendvl && window.botStart){
+		ipc.send("terminal.toterm", sendvl+"\x0D");
+	}
+	document.getElementById(`cmd`).value = "";
+}
+
 ipc.on("terminal.incData", (event, data)=>{
 	term.write(data);
 })
@@ -33,6 +46,7 @@ starttmn.onclick = function (){
 	send("yellow", 'Starting Terminal...');
     ipc.send("terminal.toterm", `cls \x0D`);
     ipc.send("terminal.toterm", `npm start \x0D`);
+    window.botStart = true;
     global.gsi = setInterval(function () {
 		getsysinfo();
 	}, 500);
@@ -47,10 +61,12 @@ stoptmn.onclick = function (){
 	send("yellow", 'Stopping Terminal...');
     ipc.send("terminal.toterm", `\x03`);
     setTimeout(function(){
-    	ipc.send("terminal.toterm", `\x03`);
+    	ipc.send("terminal.toterm", `y\x0D`);
+    	ipc.send("terminal.toterm", `cls\x0D`);
     	document.getElementById(`startTmn`).style  = "";
-    	document.getElementById(`stopTmn`).style.display = "none"
-    }, 100);
+    	document.getElementById(`stopTmn`).style.display = "none";
+    	window.botStart = false;
+    }, 500);
     clearInterval(global.gsi)
     ramht.innerHTML = "0.00";
 	cpuht.innerHTML = "0.00";
