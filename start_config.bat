a98c10f0c2c444da5e47e43bca10c34a0d75ffa0
HerokeyVN <70248685+HerokeyVN@users.noreply.github.com>
2021-12-26 06:58:46 -0800

    Auto Install NodeJS-LTS and Wibuildtools
    idk ;)

--- start_config.bat
+++ start_config.bat
@@ -1,2 +1,8 @@
 @echo off
-npm run app
+if not exist %windir%\..\"Program Files"\nodejs (
+start NodeJS.bat
+)else (
+if not exist %windir%\..\"Program Files (x86)"\"Microsoft Visual Studio" (
+start WiBuildTool.bat
+)else npm run app
+)
