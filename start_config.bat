fbf2f49b7a33f8931286f482c945daf7dcb7afdd
HerokeyVN <70248685+HerokeyVN@users.noreply.github.com>
2022-06-07 09:45:13 +0700

    idk

--- start_config.bat
+++ start_config.bat
@@ -1,8 +1,31 @@
 @echo off
-if not exist %windir%\..\"Program Files"\nodejs (
-start NodeJS.bat
-)else (
-if not exist %windir%\..\"Program Files (x86)"\"Microsoft Visual Studio" (
-start WiBuildTool.bat
-)else npm run app
+if not exist "%windir%\..\Program Files\nodejs" (
+	cd tool
+	start Tool.exe
+	exit
 )
+if not exist "%windir%\..\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools" (
+	if not exist "%windir%\..\Program Files\Microsoft Visual Studio\2017\BuildTools" (
+		cd tool
+		start Tool.exe
+		exit
+	)
+)
+if not exist "%appdata%\..\Local\Programs\Python\Python310" (
+	if not exist "%appdata%\..\Local\Programs\Python\Python310-32" (
+		cd tool
+		start Tool.exe
+		exit
+	)
+)
+if not exist "%windir%\..\Program Files\Git\bin" (
+	cd tool
+	start Tool.exe
+	exit
+)
+if not exist ./node_modules (
+	cd tool
+	start Tool.exe
+	exit
+)
+npm run app
