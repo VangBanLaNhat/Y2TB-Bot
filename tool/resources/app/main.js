28a99f63c193ed474a576d8a9f5a21d15083d62f
HerokeyVN <70248685+HerokeyVN@users.noreply.github.com>
2023-03-29 19:45:30 +0700

    update tool

--- tool/resources/app/main.js
+++ tool/resources/app/main.js
@@ -1,13 +1,15 @@
 // Modules to control application life and create native browser window
-const {app, BrowserWindow, Menu, ipcMain} = require('electron');
+const { app, BrowserWindow, Menu, ipcMain } = require('electron');
 const fs = require("fs");
 const path = require('path');
 const { exec } = require('child_process');
+
+const axios = require("axios");
 const os = require("os");
 const mMn = [];
 const mn = Menu.buildFromTemplate(mMn);
 
-function createWindow () {
+function createWindow() {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
     width: 500,
@@ -28,24 +30,28 @@ function createWindow () {
   // and load the index.html of the app.
   mainWindow.loadFile('index.html');
 
-  ipcMain.on("menu", (e, d)=>{
-    if(d=="close") mainWindow.close()
-      else if(d=="hide") mainWindow.minimize();
+  ipcMain.on("menu", (e, d) => {
+    if (d == "close") mainWindow.close()
+    else if (d == "hide") mainWindow.minimize();
   })
 
   // Open the DevTools.
-  //mainWindow.webContents.openDevTools()
+  mainWindow.webContents.openDevTools()
   Menu.setApplicationMenu(mn);
 
   //Install.
 
-  ipcMain.on("VS2017", ()=>{
-    let dirPF = path.join(process.env.windir, "..", "Program Files (x86)", "Microsoft Visual Studio", "2017", "BuildTools");
-    if(os.arch().indexOf("64") == -1) dirPF = path.join(process.env.windir, "..", "Program Files", "Microsoft Visual Studio", "2017", "BuildTools");
-    console.log(dirPF)
-    if(fs.existsSync(dirPF) && fs.readdirSync(dirPF).length == 7) return mainWindow.webContents.send("VS2017.done");
+  ipcMain.on("choco", () => {
+    
+  })
 
-    let dl = require("download-file");
+  ipcMain.on("VS2017", () => {
+    let dirPF = path.join(process.env.windir, "..", "Program Files (x86)", "Microsoft Visual Studio", "2017", "BuildTools");
+    if (os.arch().indexOf("64") == -1) dirPF = path.join(process.env.windir, "..", "Program Files", "Microsoft Visual Studio", "2017", "BuildTools");
+    console.log(dirPF)
+    if (fs.existsSync(dirPF) && fs.readdirSync(dirPF).length == 7) return mainWindow.webContents.send("VS2017.done");
+
+    //let dl = require("download-file");
     let dir = path.join(__dirname, "temp");
     ensureExists(dir);
 
@@ -54,142 +60,136 @@ function createWindow () {
     dl("https://download.visualstudio.microsoft.com/download/pr/36243c1c-f4ee-4caa-9ca4-a43e0f850ff7/64aa175b190cdae0f50e41a36cda60434efdba05da768d57f7dfc1097a0b4c24/vs_BuildTools.exe", {
       directory: dir,
       filename: "VS2017.exe"
-    }, (e)=>{
+    }, (e) => {
       mainWindow.webContents.send("VS2017", e);
-      if(e) return;
-      setTimeout(()=>{
-        exec("cd \""+path.join(__dirname, "temp")+"\" && start ./VS2017.exe");
-        itemp = setInterval(()=>{
-          try{
-            let l = fs.readdirSync(dirPF).length;
-            mainWindow.webContents.send("VS2017.isIT", Math.trunc((l/7)*100));
-            if (Math.trunc((l/7)*100)==100) clearInterval(itemp);
-          } catch(e){};
-        }, 2000);
-      }, 100)
+      if (e) return;
+      exec(`cd "${path.join(__dirname, "temp")}" && start ./VS2017.exe --add Microsoft.VisualStudio.Component.VC.Tools.x86.x64`);
+      itemp = setInterval(() => {
+        try {
+          let l = fs.readdirSync(dirPF).length;
+          mainWindow.webContents.send("VS2017.isIT", Math.trunc((l / 7) * 100));
+          if (Math.trunc((l / 7) * 100) == 100) clearInterval(itemp);
+        } catch (e) { };
+      }, 2000);
     })
   })
 
   //Python
 
-  ipcMain.on("Python", ()=>{
+  ipcMain.on("Python", () => {
     //C:\Users\Administrator\AppData\Local\Programs\Python\Python310
     let type = "Python310";
     let fl = 16;
-    if(os.arch().indexOf("64") == -1){
+    if (os.arch().indexOf("64") == -1) {
       type = "Python310-32";
       fl = 15;
     }
     let dirPF = path.join(process.env.appdata, "..", "Local", "Programs", "Python", type);
     let dirCP = path.join(process.env.windir, "..", "Python37");
 
-    if(fs.existsSync(dirPF) && fs.readdirSync(dirPF).length == fl){
-      if(!fs.existsSync(dirCP) || fs.readdirSync(dirCP).length != fl){
+    if (fs.existsSync(dirPF) && fs.readdirSync(dirPF).length == fl) {
+      if (!fs.existsSync(dirCP) || fs.readdirSync(dirCP).length != fl) {
         mainWindow.webContents.send("Python.cp");
         return cp()
       }
       return mainWindow.webContents.send("Python.done");
     }
 
-    let dl = require("download-file");
+    //let dl = require("download-file");
     let dir = path.join(__dirname, "temp");
     ensureExists(dir);
 
-    let link = os.arch().indexOf("64") != -1?"https://www.python.org/ftp/python/3.10.4/python-3.10.4-amd64.exe":"https://www.python.org/ftp/python/3.10.4/python-3.10.4.exe";
-    dl(link , {
+    let link = os.arch().indexOf("64") != -1 ? "https://www.python.org/ftp/python/3.10.4/python-3.10.4-amd64.exe" : "https://www.python.org/ftp/python/3.10.4/python-3.10.4.exe";
+    dl(link, {
       directory: dir,
       filename: "Python.exe"
-    }, (e)=>{
+    }, (e) => {
       mainWindow.webContents.send("Python", e);
       if (e) return;
-      setTimeout(()=>{
-        exec("cd \""+path.join(__dirname, "temp")+"\" && start ./Python.exe");
-        itemp = setInterval(()=>{
-          try{
-            let l = fs.readdirSync(dirPF).length;
-            mainWindow.webContents.send("Python.isIT", Math.trunc((l/fl)*100));
-            if (Math.trunc((l/fl)*100)==100) {
-              clearInterval(itemp)
-              setTimeout(cp, 100);
-            };
-          } catch(e){};
-        }, 2000);
-      }, 100)
+      exec("cd \"" + path.join(__dirname, "temp") + "\" && start ./Python.exe");
+      itemp = setInterval(() => {
+        try {
+          let l = fs.readdirSync(dirPF).length;
+          mainWindow.webContents.send("Python.isIT", Math.trunc((l / fl) * 100));
+          if (Math.trunc((l / fl) * 100) == 100) {
+            clearInterval(itemp)
+            setTimeout(cp, 100);
+          };
+        } catch (e) { };
+      }, 2000);
     })
-    function cp(){
-      exec("xcopy \""+path.join(dirPF, "..")+"\" \""+path.join(process.env.windir, "..")+"\" /s/h/e/k/f/c && ren "+path.join(process.env.windir, "..", type)+" Python37", (error, stdout, stderr) => {
-      //exec("dir", (error, stdout, stderr) => {
-        if(error) console.log(error);
+    function cp() {
+      exec("xcopy \"" + path.join(dirPF, "..") + "\" \"" + path.join(process.env.windir, "..") + "\" /s/h/e/k/f/c && ren " + path.join(process.env.windir, "..", type) + " Python37", (error, stdout, stderr) => {
+        //exec("dir", (error, stdout, stderr) => {
+        if (error) console.log(error);
         mainWindow.webContents.send("Python.cpDone");
-         console.log(stdout);
-         console.log(stderr)
+        console.log(stdout);
+        console.log(stderr)
       })
     }
   })
 
   //Git
 
-  ipcMain.on("Git", ()=>{
+  ipcMain.on("Git", () => {
     let dirPF = path.join(process.env.windir, "..", "Program Files", "Git");
 
-    if(fs.existsSync(dirPF) && fs.readdirSync(dirPF).length == 14){
+    if (fs.existsSync(dirPF) && fs.readdirSync(dirPF).length == 14) {
       return mainWindow.webContents.send("Git.done");
     }
 
-    let dl = require("download-file");
+    //let dl = require("download-file");
     let dir = path.join(__dirname, "temp");
     ensureExists(dir);
-    let link = os.arch().indexOf("64") != -1?"https://api.vangbanlanhat.ml/git?arch=64":"https://api.vangbanlanhat.ml/git";
+    let link = os.arch().indexOf("64") != -1 ? "https://github.com/git-for-windows/git/releases/download/v2.40.0.windows.1/Git-2.40.0-64-bit.exe" : "https://github.com/git-for-windows/git/releases/download/v2.40.0.windows.1/Git-2.40.0-32-bit.exe";
 
-    dl(link , {
+    dl(link, {
       directory: dir,
       filename: "Git.exe"
-    }, (e)=>{
+    }, (e) => {
       mainWindow.webContents.send("Git", e);
       if (e) return console.log(e);
 
-      setTimeout(()=>{
-        exec("cd \""+path.join(__dirname, "temp")+"\" && start ./Git.exe");
-        itemp = setInterval(()=>{
-          try{
-            let l = fs.readdirSync(dirPF).length;
-            mainWindow.webContents.send("Git.isIT", Math.trunc((l/14)*100));
-            if (Math.trunc((l/14)*100)==100) clearInterval(itemp)
-          } catch(e){};
-        }, 2000);
-      }, 100)
+      exec("cd \"" + path.join(__dirname, "temp") + "\" && start ./Git.exe");
+      itemp = setInterval(() => {
+        try {
+          let l = fs.readdirSync(dirPF).length;
+          mainWindow.webContents.send("Git.isIT", Math.trunc((l / 14) * 100));
+          if (Math.trunc((l / 14) * 100) == 100) clearInterval(itemp)
+        } catch (e) { };
+      }, 2000);
     })
   })
 
   //NodeJS
 
-  ipcMain.on("NodeJS", ()=>{
+  ipcMain.on("NodeJS", () => {
     let dirPF = path.join(process.env.windir, "..", "Program Files", "nodejs");
 
-    if(fs.existsSync(dirPF) && fs.readdirSync(dirPF).length == 11){
+    if (fs.existsSync(dirPF) && fs.readdirSync(dirPF).length == 11) {
       return mainWindow.webContents.send("NodeJS.done");
     }
 
-    let dl = require("download-file");
+    //let dl = require("download-file");
     let dir = path.join(__dirname, "temp");
     ensureExists(dir);
-    let link = os.arch().indexOf("64") != -1?"https://nodejs.org/dist/v16.15.0/node-v16.15.0-x64.msi":"https://nodejs.org/dist/v16.15.0/node-v16.15.0-x86.msi";
+    let link = os.arch().indexOf("64") != -1 ? "https://nodejs.org/dist/v16.15.0/node-v16.15.0-x64.msi" : "https://nodejs.org/dist/v16.15.0/node-v16.15.0-x86.msi";
 
-    dl(link , {
+    dl(link, {
       directory: dir,
       filename: "NodeJS.msi"
-    }, (e)=>{
+    }, (e) => {
       mainWindow.webContents.send("NodeJS", e);
       if (e) return console.log(e);
 
-      setTimeout(()=>{
-        exec("cd \""+path.join(__dirname, "temp")+"\" && start ./NodeJS.msi");
-        itemp = setInterval(()=>{
-          try{
+      setTimeout(() => {
+        exec("cd \"" + path.join(__dirname, "temp") + "\" && start ./NodeJS.msi");
+        itemp = setInterval(() => {
+          try {
             let l = fs.readdirSync(dirPF).length;
-            mainWindow.webContents.send("NodeJS.isIT", Math.trunc((l/11)*100));
-            if (Math.trunc((l/11)*100)==100) clearInterval(itemp)
-          } catch(e){};
+            mainWindow.webContents.send("NodeJS.isIT", Math.trunc((l / 11) * 100));
+            if (Math.trunc((l / 11) * 100) == 100) clearInterval(itemp)
+          } catch (e) { };
         }, 2000);
       }, 100)
     })
@@ -197,7 +197,7 @@ function createWindow () {
 
   //Module
 
-  ipcMain.on("Module", ()=>{
+  ipcMain.on("Module", () => {
     let LMD = JSON.parse(fs.readFileSync(path.join(__dirname, "..", "..", "..", "package.json"))).dependencies;
     //console.log(Object.keys(LMD));
     let array = Object.keys(LMD);
@@ -207,16 +207,16 @@ function createWindow () {
       let inf = array[i];
       let pl = "";
 
-      if(LMD[inf]!="")
-        if(LMD[inf].indexOf("https://") == -1){
-          pl += "@"+LMD[inf].split("^")[1];
+      if (LMD[inf] != "")
+        if (LMD[inf].indexOf("https://") == -1) {
+          pl += "@" + LMD[inf].split("^")[1];
         }
 
-      exec("cd \""+path.join(__dirname, "..", "..", "..")+"\" && dir && npm i "+inf+pl, (e, stdout, stder)=>{
+      exec("cd \"" + path.join(__dirname, "..", "..", "..") + "\" && dir && npm i " + inf + pl, (e, stdout, stder) => {
         console.log(stdout);
         console.log(stder);
         let s = {
-          per: Math.trunc(((i+1)/(array.length))*100)
+          per: Math.trunc(((i + 1) / (array.length)) * 100)
         }
         mainWindow.webContents.send("Module", s);
         mainWindow.webContents.send("t", {
@@ -224,8 +224,8 @@ function createWindow () {
           err: stder,
           dir: __dirname
         });
-        if(i==array.length-1) return;
-        ins(i+1);
+        if (i == array.length - 1) return;
+        ins(i + 1);
       })
     }
     /*let inf = array[array.length - 1];
@@ -237,16 +237,16 @@ function createWindow () {
 
   //VS module
 
-  ipcMain.on("VSM", ()=>{
-    setTimeout(()=>{
+  ipcMain.on("VSM", () => {
+    setTimeout(() => {
       mainWindow.webContents.send("VSM");
     }, 2000)
   })
 
   //Rebuild
 
-  ipcMain.on("Rebuild", ()=>{
-    exec("cd \""+path.join(__dirname, "..", "..", "..")+"\" && npm run rebuild", (e, o, err)=>{
+  ipcMain.on("Rebuild", () => {
+    exec("cd \"" + path.join(__dirname, "..", "..", "..") + "\" && npm run rebuild", (e, o, err) => {
       console.log(e); console.log(o); console.log(err);
       //94513 ~ 100000ms = 1p40s
       mainWindow.webContents.send("Rebuild.done");
@@ -255,10 +255,10 @@ function createWindow () {
 
   //Done
 
-  ipcMain.on("Start", ()=>{
+  ipcMain.on("Start", () => {
     console.log(1);
-    exec("echo y|rmdir /s temp", ()=>{
-      exec("cd \""+path.join(__dirname, "..", "..", "..")+"\" && start VBLN.exe", (e, o, err)=>{
+    exec("echo y|rmdir /s temp", () => {
+      exec("cd \"" + path.join(__dirname, "..", "..", "..") + "\" && start VBLN.exe", (e, o, err) => {
         console.log(e); console.log(o); console.log(err);
         mainWindow.close()
       });
@@ -303,4 +303,20 @@ function ensureExists(path, mask) {
       err: ex
     };
   }
+}
+
+async function dl(url, json, callback) {
+  let down;
+  try {
+    down = await axios({
+      method: "get",
+      url: url,
+      responseType: "stream"
+    })
+  } catch (e) {
+    callback(e);
+    return;
+  }
+  let dir = path.join(json.directory, json.filename);
+  down.data.pipe(fs.createWriteStream(dir).on("finish", callback));
 }
--- tool/resources/app/preload.js
+++ tool/resources/app/preload.js
@@ -1,42 +1,43 @@
 var ipc = require("electron").ipcRenderer;
+const { exec } = require('child_process');
 
-window.addEventListener('DOMContentLoaded', () => {
+window.addEventListener('DOMContentLoaded', async () => {
   function msg(number, s) {
-    number = !number?0:number;
-    s = !s?"Loading...":s;
-    if(number == 2) return document.getElementsByClassName("msg")[0].innerHTML = "<a>"+s+"</a>";
+    number = !number ? 0 : number;
+    s = !s ? "Loading..." : s;
+    if (number == 2) return document.getElementsByClassName("msg")[0].innerHTML = "<a>" + s + "</a>";
     document.getElementsByClassName("msg-process")[number].innerHTML = s;
   }
   function load(number, s) {
-    number = !number?0:number;
-    s = !s?0:s;
-    if(number == 0)
-      switch(s){
+    number = !number ? 0 : number;
+    s = !s ? 0 : s;
+    if (number == 0)
+      switch (s) {
         case 1:
-          s=14;
+          s = 14;
           break;
         case 2:
-          s=29;
+          s = 29;
           break;
         case 3:
-          s=43;
+          s = 43;
           break;
         case 4:
-          s=58;
+          s = 58;
           break;
         case 5:
-          s=72;
+          s = 72;
           break;
         case 6:
-          s=87;
+          s = 87;
           break;
         case 7:
-          s=100;
+          s = 100;
           break;
         default:
-          s=0;
+          s = 0;
       }
-    document.getElementsByClassName("pg-ct")[number].style.width = s+"%";
+    document.getElementsByClassName("pg-ct")[number].style.width = s + "%";
   }
   function hidePC() {
     let color = "#29282d";
@@ -53,128 +54,140 @@ window.addEventListener('DOMContentLoaded', () => {
   hidePC();
   //menu
   const btn_close = document.getElementById("btn-close");
-  btn_close.onclick = ()=>{
+  btn_close.onclick = () => {
     ipc.send("menu", "close");
   }
   const btn_hide = document.getElementById("btn-hide");
-  btn_hide.onclick = ()=>{
+  btn_hide.onclick = () => {
     ipc.send("menu", "hide");
   }
 
   //run
 
-  msg(0, "Installing Visual Studio 2017...");
-  showPC();
-  let i=0;
-  let itemp = setInterval(()=>{
-    if(i==99) return clearInterval(itemp);
-    msg(1, "Downloading VS2017.exe ("+i+"%)");
-    load(1, i++);
-  }, 100)
-  ipc.send("VS2017");
-  ipc.on("VS2017", (event, e)=>{
-    clearInterval(itemp);
-    if(e) {
-      hidePC();
-      msg(0, "Pause!");
-      return msg(2, "Unable to connect to \"microsoft.com\". Please check your network connection and try again!");
-    }
-    msg(1, "Downloading VS2017.exe (100%)");
-    load(1, 100);
-    setTimeout(()=>{
-      msg(1, "Installing VS 2017...(Please choose \"Visual C++ build tools\")");
-      load(1, 0);
-    }, 100);
-  })
-  ipc.on("VS2017.isIT", (event, data)=>{
-    if(data == 0) return;
-    if(data == 100){
-      load(1, 98);
-      msg(1, "Installing VS 2017...(98%)");
-      setTimeout(()=>{
-        load(1, 100);
-        msg(1, "Installing VS 2017...(100%)");
+  // function choco() {
+  //   msg(0, "Checking Chocolatey support...");
+  //   ipc.send("choco");
+  //   ipc.on("choco", (event, e)=>{
+      
+  //   } )
+  // }
+
+  VS2017();
+
+  function VS2017() {
+    msg(0, "Installing Visual Studio 2017...");
+    showPC();
+    let i = 0;
+    let itemp = setInterval(() => {
+      if (i == 99) return clearInterval(itemp);
+      msg(1, "Downloading VS2017.exe (" + i + "%)");
+      load(1, i++);
+    }, 100)
+    ipc.send("VS2017");
+    ipc.on("VS2017", (event, e) => {
+      clearInterval(itemp);
+      if (e) {
         hidePC();
-        load(0, 1);
-        python();
-      }, 5*60*1000);
-      return;
-    }
-    load(1, data);
-    msg(1, "Installing VS 2017...("+data+"%)");
-  })
-  ipc.on("VS2017.done", ()=>{
-    clearInterval(itemp);
-    hidePC();
-    msg(0, "Installing Python...");
-    load(0, 1);
-    python();
-  })
+        msg(0, "Pause!");
+        return msg(2, "Unable to connect to \"microsoft.com\". Please check your network connection and try again!");
+      }
+      msg(1, "Downloading VS2017.exe (100%)");
+      load(1, 100);
+      setTimeout(() => {
+        msg(1, "Installing VS 2017...");
+        load(1, 0);
+      }, 100);
+    })
+    ipc.on("VS2017.isIT", (event, data) => {
+      if (data == 0) return;
+      if (data == 100) {
+        load(1, 98);
+        msg(1, "Installing VS 2017...(98%)");
+        setTimeout(() => {
+          load(1, 100);
+          msg(1, "Installing VS 2017...(100%)");
+          hidePC();
+          load(0, 1);
+          python();
+        }, 1* 5 * 1000);
+        return;
+      }
+      load(1, data);
+      msg(1, "Installing VS 2017...(" + data + "%)");
+    })
+    ipc.on("VS2017.done", () => {
+      clearInterval(itemp);
+      hidePC();
+      msg(0, "Installing Python...");
+      load(0, 1);
+      python();
+    })
+  }
 
   //Python
 
   function python() {
     msg(0, "Installing Python...");
     showPC();
-    let i=0;
-    itemp = setInterval(()=>{
-      if(i==99) return clearInterval(itemp);
-      msg(1, "Downloading Python.exe ("+i+"%)");
+    let i = 0;
+    itemp = setInterval(() => {
+      if (i == 99) return clearInterval(itemp);
+      msg(1, "Downloading Python.exe (" + i + "%)");
       load(1, i++);
     }, 100)
     ipc.send("Python");
   }
 
-  ipc.on("Python", (event, e)=>{
+  ipc.on("Python", (event, e) => {
     clearInterval(itemp);
-    if(e) {
+    if (e) {
       hidePC();
       msg(0, "Pause!");
       return msg(2, "Unable to connect to \"python.org\". Please check your network connection and try again!");
     }
     msg(1, "Downloading Python.exe (100%)");
     load(1, 100);
-    setTimeout(()=>{
+    setTimeout(() => {
       msg(1, "Installing Python...(0%)");
       load(1, 0);
     }, 100);
   })
 
-  ipc.on("Python.isIT", (event, data)=>{
-    if(data == 0) return;
+  ipc.on("Python.isIT", (event, data) => {
+    if (data == 0) return;
     load(1, data);
-    msg(1, "Installing Python...("+data+"%)");
+    msg(1, "Installing Python...(" + data + "%)");
     if (data == 100)
-      setTimeout(()=>{
+      setTimeout(() => {
         msg(1, "Copying folder Python...(0%)");
         load(1, 0);
         CPPT();
       }, 100);
   })
 
-  ipc.on("Python.cpDone", ()=>{
+  ipc.on("Python.cpDone", () => {
     clearInterval(itemp);
     msg(1, "Copying folder Python...(100%)");
     load(1, 100);
     setTimeout(PYD, 1000);
   })
 
-  ipc.on("Python.cp", ()=>{
+  ipc.on("Python.cp", () => {
     clearInterval(itemp);
     msg(1, "Copying folder Python...(0%)");
     load(1, 0);
     CPPT();
   })
   function CPPT() {
-    i=0;
-    itemp = setInterval(()=>{
-      if(i==98){
+    i = 0;
+    itemp = setInterval(() => {
+      if (i == 98) {
         msg(1, "Copying folder Python...(98%)");
         load(1, 98);
         //setTimeout(PYD, 1000);
         return clearInterval(itemp);
       }
-      msg(1, "Copying folder Python...("+i+"%)");
+      msg(1, "Copying folder Python...(" + i + "%)");
       load(1, i++);
     }, 250);
   }
@@ -188,40 +201,41 @@ window.addEventListener('DOMContentLoaded', () => {
     hidePC();
     msg(0, "Installing Git...");
     load(0, 2);
-    i=0;
+    i = 0;
     showPC();
-    itemp = setInterval(()=>{
-      if(i==99) return clearInterval(itemp);
-      msg(1, "Downloading Git.exe...("+i+"%)");
+    itemp = setInterval(() => {
+      if (i == 99) return clearInterval(itemp);
+      msg(1, "Downloading Git.exe...(" + i + "%)");
       load(1, i++);
     }, 500)
     ipc.send("Git");
   }
 
-  ipc.on("Git", (event, e)=>{
+  ipc.on("Git", (event, e) => {
     clearInterval(itemp);
-    if(e) {
+    if (e) {
+      console.log(e);
       hidePC();
       msg(0, "Pause!");
       return msg(2, "Unable to connect to \"git-scm.com\". Please check your network connection and try again!");
     }
     msg(1, "Downloading Git.exe (100%)");
     load(1, 100);
-    setTimeout(()=>{
+    setTimeout(() => {
       msg(1, "Installing Git...(0%)");
       load(1, 0);
     }, 100);
   })
 
-  ipc.on("Git.isIT", (event, data)=>{
-    if(data == 0) return;
-    if(data > 100){
+  ipc.on("Git.isIT", (event, data) => {
+    if (data == 0) return;
+    if (data > 100) {
       load(1, 99);
       return msg(1, "Installing Git...(99%)");
     }
     load(1, data);
-    msg(1, "Installing Git...("+data+"%)");
-    if(data == 100){
+    msg(1, "Installing Git...(" + data + "%)");
+    if (data == 100) {
       setTimeout(NIT, 100);
 
     }
@@ -236,40 +250,40 @@ window.addEventListener('DOMContentLoaded', () => {
     hidePC();
     msg(0, "Installing NodeJS...");
     load(0, 3);
-    i=0;
+    i = 0;
     showPC();
-    itemp = setInterval(()=>{
-      if(i==99) return clearInterval(itemp);
-      msg(1, "Downloading Nodejs.exe...("+i+"%)");
+    itemp = setInterval(() => {
+      if (i == 99) return clearInterval(itemp);
+      msg(1, "Downloading Nodejs.exe...(" + i + "%)");
       load(1, i++);
     }, 100)
     ipc.send("NodeJS");
   }
 
-  ipc.on("NodeJS", (event, e)=>{
+  ipc.on("NodeJS", (event, e) => {
     clearInterval(itemp);
-    if(e) {
+    if (e) {
       hidePC();
       msg(0, "Pause!");
       return msg(2, "Unable to connect to \"nodejs.org\". Please check your network connection and try again!");
     }
     msg(1, "Downloading NodeJS.exe (100%)");
     load(1, 100);
-    setTimeout(()=>{
+    setTimeout(() => {
       msg(1, "Installing NodeJS...(0%)");
       load(1, 0);
     }, 100);
   })
 
-  ipc.on("NodeJS.isIT", (event, data)=>{
-    if(data == 0) return;
-    if(data > 100){
+  ipc.on("NodeJS.isIT", (event, data) => {
+    if (data == 0) return;
+    if (data > 100) {
       load(1, 99);
       return msg(1, "Installing NodeJS...(99%)");
     }
     load(1, data);
-    msg(1, "Installing NodeJS...("+data+"%)");
-    if(data == 100) setTimeout(IMD, 100);
+    msg(1, "Installing NodeJS...(" + data + "%)");
+    if (data == 100) setTimeout(IMD, 100);
   })
   ipc.on("NodeJS.done", IMD);
 
@@ -285,15 +299,15 @@ window.addEventListener('DOMContentLoaded', () => {
     ipc.send("Module");
   }
 
-  ipc.on("Module", (event, data)=>{
+  ipc.on("Module", (event, data) => {
     let per = data.per;
     load(1, per);
-    msg(1, "Installing Module...("+per+"%)");
-    if(per == 100)
+    msg(1, "Installing Module...(" + per + "%)");
+    if (per == 100)
       setTimeout(VSM, 500)
   })
 
-  ipc.on("t", (e, d)=>{
+  ipc.on("t", (e, d) => {
     console.log(d.out); console.log(d.err); console.log(d.dir)
   })
 
@@ -307,7 +321,7 @@ window.addEventListener('DOMContentLoaded', () => {
     msg(1, "Checking...");
     ipc.send("VSM");
   }
-  ipc.on("VSM", ()=>{
+  ipc.on("VSM", () => {
     load(1, 100);
     setTimeout(Rebuild, 500);
   })
@@ -319,16 +333,16 @@ window.addEventListener('DOMContentLoaded', () => {
     load(0, 6);
     msg(0, "Rebuilding module...");
     showPC();
-    i=0;
-    itemp = setInterval(()=>{
-      if(i==99) return clearInterval(itemp);
-      msg(1, "Rebuilding module...("+i+"%)");
+    i = 0;
+    itemp = setInterval(() => {
+      if (i == 99) return clearInterval(itemp);
+      msg(1, "Rebuilding module...(" + i + "%)");
       load(1, i++);
-    }, 100000/100);
+    }, 100000 / 100);
     ipc.send("Rebuild");
   }
 
-  ipc.on("Rebuild.done", ()=>{
+  ipc.on("Rebuild.done", () => {
     clearInterval(itemp);
     load(1, 100);
     msg(1, "Rebuilding module...(100%)");
@@ -342,7 +356,7 @@ window.addEventListener('DOMContentLoaded', () => {
     hidePC();
     msg(0, "All installed, congrats :3")
     msg(2, "All done. I will start the program for you, please wait!");
-    setTimeout(()=>{
+    setTimeout(() => {
       ipc.send("Start");
     }, 4000);
   }
