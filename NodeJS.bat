4d6c0b38bd7b4e0f08af8e58488b7d7bf7406dd8
HerokeyVN <70248685+HerokeyVN@users.noreply.github.com>
2024-06-08 11:19:56 +0700

    Update NodeJS.bat

--- NodeJS.bat
+++ NodeJS.bat
@@ -1,6 +1,3 @@
 @echo off
 "%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1')); choco upgrade -y nodejs-lts
 cls
-if not exist %windir%\..\"Program Files (x86)"\"Microsoft Visual Studio" (
-start WiBuildTool.bat
-)else start Y2TB.exe
